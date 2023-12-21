730a7bcbc9370f80b5307ead8eefa0750bbfd6b0
OA Wu <oawu.tw@gmail.com>
2023-12-21 09:13:36 +0800

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -30,19 +30,95 @@ Load.Vue({
               .role-units
                 .role-unit
                   header
-                    b => *text='登入前台'
+                    i._icon-api
+                    div
+                      b => *text='登入前台'
+                      span => *text='用戶使用 E-Mail 登入'
+                    label
+                  
+                  .body.api
+                    .method
+                      .row
+                        span => *text='方式'
+                        b => *text='POST'
+
+                    .header
+                      label.row._open
+                        span => *text='標題'
+
+                      .kvs
+                        .kv
+                          label => *text='Authorization'
+                          label => *text='Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9kZXYtYXBpLmlkcmlwLmNvZmZlZVwvYXV0aFwvbG9naW4iLCJpYXQiOjE3MDI4ODc1NzEsImV4cCI6MTcwODA3MTU3MSwibmJmIjoxNzAyODg3NTcxLCJqdGkiOiJWbWVJY1NXakZxaVpFZEZCIiwic3ViIjo1NjE0LCJwcnYiOiJhZmQwZmQ5YmRkOWFjNzJmZjM5ODM0MWYxZDYyODQwY2JmNGM3MTY3In0.xcmOcBh3n-JsoVNSICdS8LkOWYHpLFiYbUirmnN3mp0'
+
+                    .payload
+                      label.row._open
+                        span => *text='內文'
+                        b => *text='表單'
+                      .kvs
+                        .kv
+                          label => *text='type'
+                          label => *text='mobile'
+
+                        .kv
+                          label => *text='username'
+                          label => *text='oa.wu@idrip.coffee'
+
+                        .kv
+                          label => *text='password'
+                          label => *text='123'
+
+
                 .role-unit
                   header
-                    b => *text='清除購物車'
+                    i._icon-ifelse
+                    div
+                      b => *text='檢查是否可被申請退貨'
+                      span
+                    label
+                  
+                  .body
+
                 .role-unit
                   header
-                    b => *text='加入購物車'
+                    i._icon-api
+                    div
+                      b => *text='清除購物車'
+                      span
+                    label
+                  
+                  .body
+
                 .role-unit
                   header
-                    b => *text='取得付款憑證'
+                    i._icon-api
+                    div
+                      b => *text='加入購物車'
+                      span
+                    label
+                  
+                  .body
+
                 .role-unit
                   header
-                    b => *text='付款'
+                    i._icon-api
+                    div
+                      b => *text='取得付款憑證'
+                      span
+                    label
+                  
+                  .body
+
+                .role-unit
+                  header
+                    i._icon-api
+                    div
+                      b => *text='付款'
+                      span
+                    label
+                  
+                  .body
+
               .role-units
             
             .unit-roles
@@ -50,40 +126,117 @@ Load.Vue({
               .role-units
                 .role-unit
                   header
-                    b => *text='登入後台'
+                    i._icon-api
+                    div
+                      b => *text='登入後台'
+                      span
+                    label
+                  
+                  .body
+
                 .role-unit
                   header
-                    b => *text='排程過 12 小時'
+                    i._icon-api
+                    div
+                      b => *text='排程過 12 小時'
+                      span
+                    label
+                  
+                  .body
+
                 .role-unit
                   header
-                    b => *text='登入後台'
+                    i._icon-api
+                    div
+                      b => *text='登入後台'
+                      span
+                    label
+                  
+                  .body
+
                 .role-unit
                   header
-                    b => *text='檢視訂單-取的項目'
+                    i._icon-api
+                    div
+                      b => *text='檢視訂單-取的項目'
+                      span
+                    label
+                  
+                  .body
+
                 .role-unit
                   header
-                    b => *text='建立出貨單'
+                    i._icon-api
+                    div
+                      b => *text='建立出貨單'
+                      span
+                    label
+                  
+                  .body
+
                 .role-unit
                   header
-                    b => *text='檢視訂單-取得出貨單'
+                    i._icon-api
+                    div
+                      b => *text='檢視訂單-取得出貨單'
+                      span
+                    label
+                  
+                  .body
+
                 .role-unit
                   header
-                    b => *text='送達'
+                    i._icon-api
+                    div
+                      b => *text='送達'
+                      span
+                    label
+                  
+                  .body
+
             
             .unit-roles
               .role-units
                 .role-unit
                   header
-                    b => *text='登入前台'
+                    i._icon-api
+                    div
+                      b => *text='登入前台'
+                      span
+                    label
+                  
+                  .body
+
                 .role-unit
                   header
-                    b => *text='檢視訂單'
+                    i._icon-api
+                    div
+                      b => *text='檢視訂單'
+                      span
+                    label
+                  
+                  .body
+
                 .role-unit
                   header
-                    b => *text='檢查是否可被申請退貨'
+                    i._icon-ifelse
+                    div
+                      b => *text='檢查是否可被申請退貨'
+                      span
+                    label
+                  
+                  .body
+
                 .role-unit
                   header
-                    b => *text='申請退貨'
+                    i._icon-api
+                    div
+                      b => *text='申請退貨'
+                      span
+                    label
+                  
+                  .body
+
               .role-units
             
             .unit-roles
@@ -91,10 +244,24 @@ Load.Vue({
               .role-units
                 .role-unit
                   header
-                    b => *text='登入後台'
+                    i._icon-api
+                    div
+                      b => *text='登入後台'
+                      span
+                    label
+                  
+                  .body
+
                 .role-unit
                   header
-                    b => *text='允許退貨'
+                    i._icon-api
+                    div
+                      b => *text='允許退貨'
+                      span
+                    label
+                  
+                  .body
+
                 
             .unit-roles
               .role-units
@@ -102,10 +269,24 @@ Load.Vue({
                 .unit
                   .role-unit
                     header
-                      b => *text='登入後台'
+                      i
+                      div
+                        b => *text='登入後台'
+                        span
+                      label
+                    
+                    .body
+
                   .role-unit
                     header
-                      b => *text='退款'
+                      i
+                      div
+                        b => *text='退款'
+                        span
+                      label
+                    
+                    .body
+
 
       `
 })
