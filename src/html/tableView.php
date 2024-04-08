e434b018a06a96da79e276f7157c2253dc199aca
OA Wu <oawu.tw@gmail.com>
2024-04-08 11:52:42 +0800

    no message

--- src/html/tableView.php
+++ src/html/tableView.php
@@ -2,7 +2,7 @@
 
 /**
  * @author      OA Wu <oawu.tw@gmail.com>
- * @copyright   Copyright (c) 2015 - 2022, LaliloCore
+ * @copyright   Copyright (c) 2015 - 2024, Lalilo
  * @license     http://opensource.org/licenses/MIT  MIT License
  * @link        https://www.ioa.tw/
  */
--- src/js/tableView.js
+++ src/js/tableView.js
@@ -1,6 +1,6 @@
 /**
  * @author      OA Wu <oawu.tw@gmail.com>
- * @copyright   Copyright (c) 2015 - 2022, Lalilo
+ * @copyright   Copyright (c) 2015 - 2024, Lalilo
  * @license     http://opensource.org/licenses/MIT  MIT License
  * @link        https://www.ioa.tw/
  */
