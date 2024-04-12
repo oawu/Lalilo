798e9e7cc61e6ba4790d9fa3732e05039d233094
OA Wu <oawu.tw@gmail.com>
2024-04-12 16:14:21 +0800

    🚀 部署！

---
+++ js/jq-index.js
@@ -0,0 +1,10 @@
+/**
+ * @author      OA Wu <oawu.tw@gmail.com>
+ * @copyright   Copyright (c) 2015 - 2024, Lalilo
+ * @license     http://opensource.org/licenses/MIT  MIT License
+ * @link        https://www.ioa.tw/
+ */
+
+$(_ => {
+
+})
