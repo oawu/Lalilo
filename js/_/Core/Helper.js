92c69e61c3dba27e18b8fe464f0f5d35fb848332
OA Wu <oawu.tw@gmail.com>
2025-03-25 09:43:22 +0000

    🚀 部署！

---
+++ js/_/Core/Helper.js
@@ -0,0 +1,65 @@
+/**
+ * @author      OA Wu <oawu.tw@gmail.com>
+ * @copyright   Copyright (c) 2015 - 2025, Lalilo
+ * @license     http://opensource.org/licenses/MIT  MIT License
+ * @link        https://www.ioa.tw/
+ */
+
+const Helper = {
+  numFormat: (n, d = 3) => {
+    const w = []
+    for (let i = 0, a = ('' + n).split('').reverse(); i < a.length; i++) {
+      if (!i || (i % d)) {
+        w.push(a[i])
+      } else {
+        w.push(',', a[i])
+      }
+    }
+    return w.reverse().join('')
+  },
+  pad0: (t, n = 2, c = '0') => {
+    t = '' + t
+    c = '' + c
+    n = '' + Math.pow(10, n - 1)
+    if (t.length > n.length) {
+      return t
+    }
+    n = n.length - t.length
+    return c.repeat(n) + t
+  },
+  date: (format = 'Y-m-d H:i:s', now = new Date()) => {
+    if (format instanceof Date) {
+      now = format
+      format = typeof now == 'string' ? now : 'Y-m-d H:i:s'
+    }
+
+    now = now instanceof Date
+      ? now
+      : new Date(now * 1000)
+
+    return format.replace('Y', now.getFullYear()).replace('m', Helper.pad0(now.getMonth() + 1)).replace('d', Helper.pad0(now.getDate())).replace('H', Helper.pad0(now.getHours())).replace('i', Helper.pad0(now.getMinutes())).replace('s', Helper.pad0(now.getSeconds()))
+  },
+  copyStr: (text, done, fail) => {
+    let error = null
+    let el = document.createElement('textarea')
+    el.className = '__lalilo_copy_str'
+    el.value = text
+    document.body.appendChild(el)
+    el.select()
+
+    try {
+      document.execCommand('copy')
+    } catch (e) {
+      error = e.message
+    }
+
+    document.body.removeChild(el)
+
+    el = null
+    let func = error
+      ? fail
+      : done
+
+    typeof func == 'function' && func()
+  }
+}
---
+++ js/index.js
@@ -0,0 +1,41 @@
+/**
+ * @author      OA Wu <oawu.tw@gmail.com>
+ * @copyright   Copyright (c) 2015 - 2025, Lalilo
+ * @license     http://opensource.org/licenses/MIT  MIT License
+ * @link        https://www.ioa.tw/
+ */
+
+Load.Vue({
+  data: {
+    style: {
+      color: 'rgba(120, 120, 120, 1.00);'
+    },
+    version: '2.0.1'
+  },
+  mounted () {
+  },
+  computed: {
+  },
+  methods: {
+    date (format) {
+      const pad0 = t => (t < 10 ? '0' : '') + t
+      const date = new Date()
+      return format.replace('Y', date.getFullYear())
+        .replace('m', pad0(date.getMonth() + 1))
+        .replace('d', pad0(date.getDate()))
+        .replace('H', pad0(date.getHours()))
+        .replace('i', pad0(date.getMinutes()))
+        .replace('s', pad0(date.getSeconds()))
+    }
+  },
+  template: `
+  main#app.a
+    h1 => *text='你好，世界！'
+    div => :style=style
+      span => *text='這是 '
+      b    => *text='Lalilo'
+      span => *text='，你目前版本是 '
+      b    => *text=version
+    br
+    span => *text=date('Y-m-d H:i:s')`
+})
