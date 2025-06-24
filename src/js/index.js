af0243b395b0cfa47e01fb4bb562e40b38bf71fd
OA Wu <oawu.tw@gmail.com>
2025-06-24 16:30:33 +0800

    no message

--- src/js/index.js
+++ src/js/index.js
@@ -5,31 +5,35 @@
  * @link        https://www.ioa.tw/
  */
 
-window.Load.Vue({
-  data: {
-    style: {
-      color: 'rgba(120, 120, 120, 1.00);'
+window.Load.Vue(_ => {
+  const { Str } = window.Helper
+
+  return {
+    data: {
+      style: {
+        color: 'rgba(120, 120, 120, 1.00);'
+      },
+      version: '3.0.1'
     },
-    version: '3.0.1'
-  },
-  async mounted () {
-  },
-  computed: {
-    date () {
-      return window.Helper.Str.date()
-    }
-  },
-  methods: {
-  },
-  template: `
-    main#app
-      h1 => *text='你好，世界！'
-      div => :style=style
-        span => *text='這是 '
-        b    => *text='Lalilo'
-        span => *text='，你目前版本是 '
-        b    => *text=version
-      br
-      span => *text=date
-    `
+    async mounted () {
+    },
+    computed: {
+      date () {
+        return Str.date()
+      }
+    },
+    methods: {
+    },
+    template: `
+      main#app
+        h1 => *text='你好，世界！'
+        div => :style=style
+          span => *text='這是 '
+          b    => *text='Lalilo'
+          span => *text='，你目前版本是 '
+          b    => *text=version
+        br
+        span => *text=date
+      `
+  }
 })
